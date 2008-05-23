809eecadf5a42adda18835184b978bfeeb5bbe68
Colin Casey <casey.colin@gmail.com>
2008-05-23 19:22:09 +0000

    - uses bulk import for faster performance

--- lib/data_warehouse/acts_as_importable.rb
+++ lib/data_warehouse/acts_as_importable.rb
@@ -8,13 +8,15 @@ module DataWarehouse
       def acts_as_importable(import_table = nil, options = {})
         unless acts_as_importable? # don't let AR call this twice
           cattr_accessor :import_from_table, :import_primary_key, :import_overwrite_existing,
-            :import_perform_validations, :import_must_import_all_or_fail, :import_column_prefix
+            :import_perform_validations, :import_must_import_all_or_fail, :import_column_prefix,
+            :import_excludes
           self.import_from_table = import_table
           self.import_primary_key = options[:primary_key] || guess_primary_key(import_table)
           self.import_perform_validations = false
           self.import_must_import_all_or_fail = options.has_key?(:must_import_all_or_fail) ? options[:must_import_all_or_fail] : true
           self.import_overwrite_existing = options.has_key?(:overwrite_existing) ? options[:overwrite_existing] : false
           self.import_column_prefix = options[:column_prefix]
+          self.import_excludes = options[:exclude] || []
           include ImportMethods
         end
       end
@@ -35,11 +37,13 @@ module DataWarehouse
       end
       
       def guess_target_attribute(target_attribute, possible_attributes, prefix = nil)
+        return target_attribute if possible_attributes.include?(target_attribute)
+        
         weighted_levenshtein_distances = possible_attributes.collect do |possible_attribute|
           word1 = target_attribute.gsub('_', '').downcase
           word2 = possible_attribute.gsub('_', '').downcase
-          word1.gsub!(prefix.downcase, '') if prefix
-          word2.gsub!(prefix.downcase, '') if prefix
+          word1.gsub!(prefix.downcase, '') if prefix && word1.starts_with?(prefix)
+          word2.gsub!(prefix.downcase, '') if prefix && word2.starts_with?(prefix)
           distance = Text::Levenshtein.distance(word1, word2)        
           { :attribute => possible_attribute, :weight => (word1.length - distance).abs.to_f / word1.length.to_f }
         end
@@ -64,54 +68,86 @@ module DataWarehouse
           self.import_transformers[original_column] = { :target_column => target_column || original_column, :block => block }
         end
         
-        def import_from_data_warehouse              
-          data_warehouse_configurations = YAML::load_file("#{RAILS_ROOT}/config/data_warehouse.yml")          
+        def import_from_data_warehouse                        
+          console = Logger.new(STDOUT)
+          console.level = Logger::INFO
+          data_warehouse_configurations = YAML::load_file("#{RAILS_ROOT}/config/data_warehouse.yml")                    
           
-          logger.debug 'connecting to data warehouse'                   
+          console.debug 'connecting to data warehouse'                   
           ActiveRecord::Base.establish_connection(data_warehouse_configurations["#{RAILS_ENV}"])    
 
           table_name = self.import_from_table
           primary_key = self.import_primary_key
-          logger.debug "reading data from #{table_name}"      
+          console.info "reading data from #{table_name}"      
           dw_table = Class.new(ActiveRecord::Base) do
             set_table_name  table_name.downcase
             set_primary_key primary_key.downcase if primary_key
           end  
           
           columns = dw_table.columns.inject(Hash.new) { |hash, col| hash.merge({col.name => col}) }
-          logger.debug "column info: #{dw_table.columns.inspect}"
+          console.debug "column info: #{dw_table.columns.inspect}"
           
-          records = dw_table.find :all                
-          logger.debug "#{records.size} records ready to be imported" 
+          records = dw_table.find :all    
+          if records.empty?
+            console.info "table is empty - nothing to import"
+            return
+          end
           
-          logger.debug 'restoring default active record connection'
+          console.info "#{records.size} records ready to be imported" 
+          
+          console.debug 'restoring default active record connection'
           ActiveRecord::Base.establish_connection(ActiveRecord::Base.configurations[RAILS_ENV])  
                   
-          logger.debug 'transforming records for import'
+          console.debug 'transforming records for import'
+          import_columns = self.columns.collect{ |column| column.name }
+          imports = []
           records.each_with_index do |record, i|
+            if self.exists?(record.id)
+              console.info "record #{i+1} already exists, skipping to next record"
+              next
+            end
+            
             attributes = {}
-            record.attributes.each do |attribute, value|                            
-              if transformer = self.import_transformers[attribute]
+            record.attributes.collect do |attribute, value|  
+              next if attribute == self.import_primary_key.downcase
+              if self.import_excludes.collect{ |exclude| exclude.downcase }.include?(attribute) 
+                logger.debug "excluding #{attribute}"
+                next
+              end
+              
+              if transformer = (self.import_transformers || {})[attribute]
                 block = transformer[:block]
-                attributes[transformer[:target_column]] = block ? block.call(attribute, value, columns[attribute]) : value
+                attributes[transformer[:target_column]] = block ? block.call(attribute, value, columns[attribute], record) : value
+                console.debug "transform: #{attribute}=#{value} -> #{transformer[:target_column]}=#{attributes[transformer[:target_column]]}"
               else
                 target_columns = self.columns.collect{ |col| col.name }
                 guessed_attribute = guess_target_attribute(attribute, target_columns, self.import_column_prefix)
                 attributes[guessed_attribute] = value
+                console.debug "transform guess: #{attribute}=#{value} -> #{guessed_attribute}=#{attributes[guessed_attribute]}"
               end
             end            
-            attributes[:id] = record.id
+            attributes[self.primary_key] = record.id
+            console.debug "setting primary key=#{record.id}"
             
-            logger.debug "importing #{self.class_name} record #{i+1} of #{records.size}"
-            begin
-              new_record = self.new(attributes)
-              new_record.id = attributes[:id]
-              saved = new_record.save(false)
-              saved
-            rescue Exception => exc
-              logger.error "IMPORT ERROR: #{exc.message} - #{attributes.inspect}"
-            end  
-          end    
+            attributes['created_at'] = DateTime.now if import_columns.include?('created_at')
+            attributes['updated_at'] = DateTime.now if import_columns.include?('updated_at')
+            
+            imports << import_columns.collect { |col| attributes[col] }
+#            console.info "importing #{self.class_name} record #{i+1} of #{records.size}"
+#            begin
+#              id = attributes.delete(:id)
+#              new_record = self.new(attributes)
+#              new_record.id = id
+#              saved = new_record.save(false)
+#              saved
+#            rescue Exception => exc
+#              console.error "IMPORT ERROR: #{exc.message} - #{attributes.inspect}"
+#            end  
+          end
+          
+          console.info 'starting bulk import'
+          self.import import_columns, imports, { :validate => false }
+          console.info "import of #{table_name} complete"
         end
       end # ClassMethods    
     end # ImportMethods
