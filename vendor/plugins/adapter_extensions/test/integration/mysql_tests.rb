e9f06459c2515b1fc7adf247aaa43c93659cdea9
Colin Casey <casey.colin@gmail.com>
2008-08-15 18:28:49 +0000

    Moved adapter_extensions and activewarehouse-etl into vendor/gems folder since they are gems, not plugins.

--- vendor/plugins/adapter_extensions/test/integration/mysql_tests.rb
+++
@@ -1,6 +0,0 @@
-module MysqlTests
-  def test_support_select_into_table_should_return_true
-    # TODO: mock connection adapter?
-    assert connection.support_select_into_table?
-  end
-end
--- vendor/plugins/adapter_extensions/test/integration/postgresql_tests.rb
+++
@@ -1,6 +0,0 @@
-module PostgresqlTests
-  def test_support_select_into_table_should_return_false
-    # TODO: mock connection adapter?
-    assert connection.support_select_into_table?
-  end
-end
