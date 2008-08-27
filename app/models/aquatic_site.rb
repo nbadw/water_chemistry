0f03afcdf5380ff0df731aa47de30a5da0aaad88
Colin Casey <casey.colin@gmail.com>
2008-08-27 03:37:00 +0000

    aquatic_site column, attribute, and alias test are passing

--- app/models/aquatic_site.rb
+++ app/models/aquatic_site.rb
@@ -35,33 +35,18 @@
 #  updated_at       :datetime        
 #
 
-class AquaticSite < ActiveRecord::Base  
-  include AquaticDataWarehouse::IncorporatedModel   
-  
-  class AquaticSiteInUse < ActiveRecord::ActiveRecordError; end  
-  
-  set_table_name  'tblaquaticsite'
-  set_primary_key 'aquaticsiteid'
-  
-  alias_attribute :name, :aquaticsitename
-  alias_attribute :description, :aquaticsitedesc
-  alias_attribute :waterbody_id, :waterbodyid
-  alias_attribute :waterbody_name, :waterbodyname
-  alias_attribute :raw_longitude, :xcoordinate
-  alias_attribute :raw_latitude, :ycoordinate
-  alias_attribute :coordinate_system_id, :coordinatesystem
-  alias_attribute :coordinate_source, :coordinatesource
-        
-  belongs_to :waterbody, :foreign_key => 'waterbodyid'
+class AquaticSite < AquaticDataWarehouse::BaseTbl
+  # ASSOCIATIONS
+  belongs_to :waterbody, :foreign_key => 'waterbody_id'
   has_many   :aquatic_site_usages
   has_many   :aquatic_activities, :through => :aquatic_site_usages, :uniq => true
   has_many   :agencies, :through => :aquatic_site_usages, :uniq => true
   
-  composed_of :recorded_location, :class_name => 'Location', :mapping => [%w(raw_latitude latitude), %w(raw_longitude longitude), %w(coordinate_system_id coordinate_system_id)]
-  composed_of :gmap_location, :class_name => 'GmapLocation', :mapping => [%w(gmap_latitude latitude), %w(gmap_longitude longitude)]  
-        
-  before_destroy :destroy_allowed?
-    
+  # VALUE OBJECTS
+  #composed_of :recorded_location, :class_name => 'Location', :mapping => [%w(raw_latitude latitude), %w(raw_longitude longitude), %w(coordinate_system_id coordinate_system_id)]
+  #composed_of :gmap_location, :class_name => 'GmapLocation', :mapping => [%w(gmap_latitude latitude), %w(gmap_longitude longitude)]  
+      
+  # VALIDATIONS    
   validates_presence_of :description, :waterbody  
   validates_each :recorded_location, :allow_blank => true do |record, attr, recorded_location|
     recorded_location.copy_errors_to(record, [:raw_latitude, :raw_longitude, :coordinate_system_id]) unless recorded_location.valid?
@@ -69,30 +54,29 @@ class AquaticSite < ActiveRecord::Base
   validates_each :gmap_location, :allow_blank => true do |record, attr, gmap_location|
     gmap_location.copy_errors_to(record, [:gmap_latitude, :gmap_longitude]) unless gmap_location.valid?
   end
-    
+  
+  # INSTANCE METHODS  
+  alias_attribute :name,          :aquatic_site_name  
+  alias_attribute :description,   :aquatic_site_desc
+  alias_attribute :raw_latitude,  :x_coordinate
+  alias_attribute :raw_longitude, :y_coordinate
+  
   def drainage_code
     self.waterbody.drainage_code if self.waterbody
-  end  
+  end    
+  
+  # throw error if delete is attempted when the site is in use by any agency    
+  class AquaticSiteInUse < ActiveRecord::ActiveRecordError; end 
+  
+  before_destroy :destroy_allowed?
   
   private   
   def destroy_allowed?
     check_if_incorporated
     check_if_in_use
-  end
-  
+  end   
+    
   def check_if_in_use
     raise(AquaticSiteInUse, "Site is in use, record cannot be deleted") unless self.aquatic_site_usages.empty?
-  end
-  
-  
-  
-  class << self
-    def name_column
-      :name
-    end
-    
-    def waterbody_id_column
-      :waterbody_id
-    end
   end
 end
--- test/unit/aquatic_site_test.rb
+++ test/unit/aquatic_site_test.rb
@@ -2,8 +2,8 @@ require File.dirname(__FILE__) + '/../test_helper'
 require 'mocha'
 
 class AquaticSiteTest < ActiveSupport::TestCase
-  should_use_table :tblaquaticsite
-  should_use_primary_key :aquaticsiteid
+  should_use_table "tblAquaticSite"
+  should_use_primary_key "AquaticSiteId"
   
   should_belong_to :waterbody
   should_have_many :aquatic_site_usages
@@ -12,63 +12,41 @@ class AquaticSiteTest < ActiveSupport::TestCase
    
   should_require_attributes :description, :waterbody
   
-  context "aquatic site model" do
-    should_have_db_column :aquaticsiteid, :type => :integer, :null => false
-    
-    should_have_db_column :oldaquaticsiteid, :type => :integer
-    
-    should_have_db_column :riversystemid, :type => :integer 
-    
-    should_have_db_column  :waterbodyid, :type => :integer 
-    should_alias_attribute :waterbodyid, :waterbody_id
+  should_have_db_column "AquaticSiteId", :type => :integer, :null => false
+  should_have_db_column "OldAquaticSiteId", :type => :integer
+  should_have_db_column "RiverSystemId", :type => :integer 
+  should_have_db_column :waterbodyid, :type => :integer 
+  should_have_db_column :waterbodyname, :type => :string, :limit => 50
+  should_have_db_column :aquaticsitename, :type => :string, :limit => 100
+  should_have_db_column :aquaticsitedesc, :type => :string, :limit => 250
+  should_have_db_column :habitatdesc, :type => :string, :limit => 50
+  should_have_db_column :reachno, :type => :integer 
+  should_have_db_column :startdesc, :type => :string, :limit => 100
+  should_have_db_column :enddesc, :type => :string, :limit => 100
+  should_have_db_column :startroutemeas, :type => :float
+  should_have_db_column :endroutemeas, :type => :float 
+  should_have_db_column :sitetype, :type => :string, :limit => 20
+  should_have_db_column :specificsiteind, :type => :string, :limit => 1
+  should_have_db_column :georeferencedind, :type => :string, :limit => 1
+  should_have_db_column :dateentered, :type => :datetime
+  should_have_db_column :incorporatedind, :type => :boolean, :default => false
+  should_have_db_column :coordinatesource, :type => :string, :limit => 50  
+  should_have_db_column :coordinatesystem, :type => :string, :limit => 50
+  should_have_db_column :xcoordinate, :type => :string, :limit => 50
+  should_have_db_column :ycoordinate,  :type => :string, :limit => 50
+  should_have_db_column :coordinateunits, :type => :string, :limit => 50
+  should_have_db_column :comments, :type => :string, :limit => 50    
+  
+  should_have_instance_methods :aquatic_site_id, :old_aquatic_site_id, :waterbody_id, :waterbody_name, 
+    :aquatic_site_name, :aquatic_site_desc, :habitat_desc, :reach_no, :start_desc, :end_desc, :start_route_meas,
+    :end_route_meas, :site_type, :specific_site_ind, :geo_referenced_ind, :date_entered, :incorporated_ind, :coordinate_source,
+    :coordinate_system, :x_coordinate, :y_coordinate, :coordinate_units, :comments
+  
+  should_alias_attribute :aquatic_site_name, :name
+  should_alias_attribute :aquatic_site_desc, :description
+  should_alias_attribute :x_coordinate, :raw_longitude
+  should_alias_attribute :y_coordinate, :raw_latitude
 
-    should_have_db_column  :waterbodyname, :type => :string, :limit => 50
-    should_alias_attribute :waterbodyname, :waterbody_name
-
-    should_have_db_column  :aquaticsitename, :type => :string, :limit => 100
-    should_alias_attribute :aquaticsitename, :name
-
-    should_have_db_column  :aquaticsitedesc, :type => :string, :limit => 250
-    should_alias_attribute :aquaticsitedesc, :description
-
-    should_have_db_column :habitatdesc, :type => :string, :limit => 50
-    
-    should_have_db_column :reachno, :type => :integer 
-    
-    should_have_db_column :startdesc, :type => :string, :limit => 100
-    
-    should_have_db_column :enddesc, :type => :string, :limit => 100
-    
-    should_have_db_column :startroutemeas, :type => :float
-    
-    should_have_db_column :endroutemeas, :type => :float 
-    
-    should_have_db_column :sitetype, :type => :string, :limit => 20
-    
-    should_have_db_column :specificsiteind, :type => :string, :limit => 1
-    
-    should_have_db_column :georeferencedind, :type => :string, :limit => 1
-    
-    should_have_db_column :dateentered, :type => :datetime
-    
-    should_have_db_column :incorporatedind, :type => :boolean, :default => false
-
-    should_have_db_column  :coordinatesource, :type => :string, :limit => 50
-    should_alias_attribute :coordinatesource, :coordinate_source
-    
-    should_have_db_column :coordinatesystem, :type => :string, :limit => 50
-
-    should_have_db_column  :xcoordinate, :type => :string, :limit => 50
-    should_alias_attribute :xcoordinate, :raw_longitude
-
-    should_have_db_column  :ycoordinate,  :type => :string, :limit => 50
-    should_alias_attribute :ycoordinate, :raw_latitude
-
-    should_have_db_column :coordinateunits, :type => :string, :limit => 50
-
-    should_have_db_column :comments, :type => :string, :limit => 50
-  end
-    
   
   should "not validate location value objects when they are blank" do
     aquatic_site = AquaticSite.spawn
