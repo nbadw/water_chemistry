cb0a33c813025da3d04221e2ac437cfc0c4aac5d
Colin Casey <casey.colin@gmail.com>
2009-06-04 21:20:36 +0000

    - added required gem config for openrain-action_mailer_tls to environment.rb - gmail account details in smtp_gmail.yml - configured mail initializer to use gmail account for mail sends

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,13 +1,20 @@
+require "smtp_tls"
+
+config_file = "#{RAILS_ROOT}/config/smtp_gmail.yml"
+raise "Sorry, you must have #{config_file}" unless File.exists?(config_file)
+config_options = YAML.load_file(config_file)
+
 # Email settings
 ActionMailer::Base.delivery_method = :smtp
+# Use the following account for email delivery
 ActionMailer::Base.smtp_settings = {
-  :address => "mail.colincasey.com",
-  :port => 26,
-  :domain => "colincasey.com",
-  :authentication => :login,
-  :user_name => "test+colincasey.com",
-  :password => "Testx3wp3"  
-}
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}.merge(config_options) # Configuration options override default options
+
+# ActionMailer settings
 class ApplicationMailer < ActionMailer::Base
   self.template_root = File.join(RAILS_ROOT, 'app', 'mailers', 'views')  
   default_url_options[:host] = 'cri.nbwaters.unb.ca'
