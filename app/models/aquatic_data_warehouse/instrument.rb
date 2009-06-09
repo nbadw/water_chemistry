41b234e3e307602fbb2c5675ba7ae30d0c035024
Colin Casey <casey.colin@gmail.com>
2009-06-09 18:13:24 +0000

    - join tables were not found because the table names lookup is case-sensitive

--- app/models/aquatic_data_warehouse/instrument.rb
+++ app/models/aquatic_data_warehouse/instrument.rb
@@ -14,7 +14,7 @@
 
 class Instrument < AquaticDataWarehouse::BaseCd
   set_primary_key 'InstrumentCd'
-  has_and_belongs_to_many :measurements, :join_table => 'cdmeasureinstrument', :foreign_key => 'InstrumentCd', :association_foreign_key => 'OandMCd'
+  has_and_belongs_to_many :measurements, :join_table => 'cdMeasureInstrument', :foreign_key => 'InstrumentCd', :association_foreign_key => 'OandMCd'
   
   def name
     instrument.to_s
--- app/models/aquatic_data_warehouse/unit_of_measure.rb
+++ app/models/aquatic_data_warehouse/unit_of_measure.rb
@@ -15,7 +15,7 @@
 class UnitOfMeasure < AquaticDataWarehouse::BaseCd
   set_table_name  'cdUnitofMeasure'
   set_primary_key 'UnitofMeasureCd'
-  has_and_belongs_to_many :measurements, :join_table => 'cdmeasureunit', :foreign_key => 'UnitofMeasureCd', :association_foreign_key => 'OandMCd'
+  has_and_belongs_to_many :measurements, :join_table => 'cdMeasureUnit', :foreign_key => 'UnitofMeasureCd', :association_foreign_key => 'OandMCd'
   
   alias_attribute :name, :unitof_measure
   alias_attribute :abbrev, :unitof_measure_abv
