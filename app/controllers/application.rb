10a20acf887fe1f5137654edc53acfb80dd4ca11
Colin Casey <casey.colin@gmail.com>
2009-09-10 17:31:48 +0000

    - added guards for filters so they don't run twice - modified how the locale is set throughout the application

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,9 +4,15 @@
 class ApplicationController < ActionController::Base
   include AuthenticatedSystem
   include HoptoadNotifier::Catcher
-  
+
+  # Activescaffold content gets delivered through components, which are derived
+  # from ApplicationController, so the components end up inheriting the following filters:
   before_filter :set_javascripts_and_stylesheets
-  around_filter :set_locale
+  before_filter :set_locale
+  # To prevent these filters from being performed twice, we can take advantage of the fact
+  # that the request object is duplicated for the component rendering, so, when the filter
+  # executes for the first time, we can store a flag indicating that it has already run, and
+  # then skip the filter logic on any further passes.
   
   ActiveScaffold.set_defaults do |config| 
     config.ignore_columns.add [:created_at, :updated_at, :created_by, :updated_by, :lock_version]
@@ -63,94 +69,57 @@ class ApplicationController < ActionController::Base
   def include_javascript(javascript, options = {})
     included_javascripts << [javascript, options]
   end  
-  
+
+  private
   def set_javascripts_and_stylesheets
+    unless request.instance_variable_get(:@javascripts_and_stylesheets_set)
+      request.instance_variable_set(:@javascripts_and_stylesheets_set, true)
+      _set_javascripts_and_stylesheets
+    end
+  end
+
+  def _set_javascripts_and_stylesheets
     include_stylesheet :base, { :merged => true }
     include_stylesheet :ie,   { :merged => true, :conditional => 'IE' }
     include_javascript :base, { :merged => true }
-    
+
     if Rails.env == 'development'
       include_javascript 'http://getfirebug.com/releases/lite/1.2/firebug-lite-compressed.js', { :conditional => 'IE', :lazy_load => true }
     end
   end
 
-  private
+  def set_locale(&block)
+    unless request.instance_variable_get(:@locale_set)
+      request.instance_variable_set(:@locale_set, true)
+      _set_locale(&block)
+    end
+  end
+
   # Set the locale from the parameters, the session, or the navigator
   # If none of these works, the Globalite default locale is set (en-*)
-  def set_locale
-    # Get the current path and request method (useful in the layout for changing the language)
-    @current_path = request.env['PATH_INFO']
-    @request_method = request.env['REQUEST_METHOD']
-
+  def _set_locale
     # Try to get the locale from the parameters, from the session, and then from the navigator
-    if params[:user_locale]
-      logger.debug "[globalite] #{params[:user_locale][:code]} locale passed"
-      Locale.code = params[:user_locale][:code] #get_matching_ui_locale(params[:user_locale][:code]) #|| session[:locale] || get_valid_lang_from_accept_header || Globalite.default_language
+    if params[:lang] && %w(en fr).include?(params[:lang])
+      logger.debug "[globalite] using language passed as parameter: #{params[:lang]}"
+      Locale.code = locale_code(params[:lang])
+      # Store the locale in the session
+      session[:locale] = Locale.code
+    elsif logged_in?
+      logger.debug "[globalite] using preferred language: #{current_user.language}"
+      Locale.code = locale_code(current_user.language)
       # Store the locale in the session
       session[:locale] = Locale.code
     elsif session[:locale]
-      logger.debug "[globalite] loading locale: #{session[:locale]} from session"
+      logger.debug "[globalite] using locale from session: #{session[:locale]}"
       Locale.code = session[:locale]
     else
-      header_lang = get_valid_lang_from_accept_header
-      logger.debug "[globalite] found a valid http header locale: #{header_lang}"
-      Locale.code = header_lang
+      logger.debug "[globalite] no locale information present - using default: #{Globalite.default_language}"
+      Locale.code = locale_code(Globalite.default_language)
     end
-
     logger.debug "[globalite] Locale set to #{Locale.code}"
-    # render the page
-    yield
-
-    # reset the locale to its default value
-    Locale.reset!
   end
 
-  # Get a sorted array of the navigator languages
-  def get_sorted_langs_from_accept_header
-    accept_langs = (request.env['HTTP_ACCEPT_LANGUAGE'] || "en-us,en;q=0.5").split(/,/) rescue nil
-    return nil unless accept_langs
-
-    # Extract langs and sort by weight
-    # Example HTTP_ACCEPT_LANGUAGE: "en-au,en-gb;q=0.8,en;q=0.5,ja;q=0.3"
-    wl = {}
-    accept_langs.each {|accept_lang|
-      if (accept_lang + ';q=1') =~ /^(.+?);q=([^;]+).*/
-        wl[($2.to_f rescue -1.0)]= $1
-      end
-    }
-    logger.debug "[globalite] client accepted locales: #{wl.sort{|a,b| b[0] <=> a[0] }.map{|a| a[1] }.to_sentence}"
-    sorted_langs = wl.sort{|a,b| b[0] <=> a[0] }.map{|a| a[1] }
-  end
-
-  # Returns a valid language that best suits the HTTP_ACCEPT_LANGUAGE request header.
-  # If no valid language can be deduced, then <tt>nil</tt> is returned.
-  def get_valid_lang_from_accept_header
-    # Get the sorted navigator languages and find the first one that matches our available languages
-    get_sorted_langs_from_accept_header.detect{|l| get_matching_ui_locale(l) }
-  end
-
-  # Returns the UI locale that best matches with the parameter
-  # or nil if not found
-  def get_matching_ui_locale(locale)
-    lang = locale[0,2].downcase
-    if locale[3,5]
-      country = locale[3,5].upcase
-      logger.debug "[globalite] trying to match locale: #{lang}-#{country}"
-      locale_code = "#{lang}-#{country}".to_sym
-    else
-      logger.debug "[globalite] trying to match #{lang}-*"
-      locale_code = "#{lang}-*".to_sym
-    end
-
-    # Check with exact matching
-    if Globalite.ui_locales.values.include?(locale)
-      logger.debug "[globalite] Globalite does include #{locale}"
-      locale_code
-    end
-
-    # Check on the language only
-    Globalite.ui_locales.values.each do |value|
-      value.to_s =~ /#{lang}-*/ ? value : nil
-    end
+  def locale_code(lang)
+    "#{lang}-*"
   end
 end
