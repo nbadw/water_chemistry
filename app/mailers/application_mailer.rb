9ce9170e3eb20c5edb923053a7707f62d88e68c2
Colin Casey <casey.colin@gmail.com>
2009-10-23 15:25:40 +0000

    - more deploy setup

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,6 +2,5 @@
 
 class ApplicationMailer < ActionMailer::Base
   self.template_root = File.join(RAILS_ROOT, 'app', 'mailers', 'views')
-  default_url_options[:host] = 'cri.nbwaters.unb.ca'
-  default_url_options[:port] = 3000
+  default_url_options[:host] = 'cri-linux.nbwaters.unb.ca'
 end
